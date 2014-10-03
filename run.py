9cdba084c77dd0b7c1424ecbc51278614996048e
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-10-02 21:13:44 -0400

    Add 2nd version of run.py

--- run.py
+++ run.py
@@ -5,28 +5,23 @@ Created on Sat Dec 14 17:24:44 2013
 @author: jmcontreras
 """
 
-# TODO:
-# Present focus is on developing a miniminally-viable board that can be played
-# with a WHILE loop to completion
-# With players and properties ready to go, you are read to build a board
-# The board will be a dictionary that guides the players through the different
-# dictionaries (streets, utilities, railroads, and other positions)
-# Rather than having multiple dictionries, I wonder if it is possible to create
-# a single one to hold all of the board information
-# One option would be to write a csv file with one row per board position, with
-# different columns filled in for each property, depending on its type
-# Then, the for loop that would bring in this documen would have an if flow
-# that would determine which kind of object gets created at that particular
-# point
-# The weakness of this approach may have to do with performing operations
-# across the dictionary (e.g., can you check that a person has all monopolies?)
-# If not, should this operatin be done at a player level?
-# Maybe player level is no good because when  player dies it takes important
-# informtion along with it
+# TODO: Allow players to play a second turn when the roll doubles
+# TODO: Attempt one play
+# TODO: Allow a player without cash to sell their properties before defaulting
+#       Check the pay method of the Player object
+# TODO: Many of the classes (Card, Chance, Chest, Jail, Idle) need work
+
+
+
+# Import modules
+import csv
+import numpy as np
+
+
 
 def get_players(n_players):
     
-    # Ensure the number number of players is acceptable
+    # Ensure the number of players is acceptable
     if n_players < 2:
         raise ValueError('A game must have at least 2 players.')
     elif n_players > 10:
@@ -42,17 +37,20 @@ def get_players(n_players):
             self.position = 0    # Board position
             self.jail_card = 0   # Number of "Get Out Of Jail Free" cards
             self.jail_turns = 0  # Number of remaining turns in jail
-            
+        
+        # Move the player across the board
         def move(self, dice_value):
             self.position += dice_value
             if self.position >= 40:
                 self.position -= 40
                 self.cash += 200
         
+        # Buy a property
         def buy(self, prop_id, price):
             self.properties.append(prop_id)
             self.cash -= price
         
+        # Pay anoter player
         def pay(self, payee, payment):
             if self.cash > payment:
                 self.cash -= payment
@@ -64,21 +62,16 @@ def get_players(n_players):
         def default(self):
             del players[self.id]
             
-    # Create and fill players list
-    players = []
-    [players.append(Player(p)) for p in range(n_players)]
-    
-    # Return players list
+    # Create and return players list
+    players = [Player(p) for p in xrange(n_players)]
     return players
 
 
-
-def get_board():
-
+def get_board(board_file):
+    
     class Property(object):
-        def __init__(self, name, property_type, position, price, rent):
+        def __init__(self, name, position, price, rent):
             self.name = name
-            self.property_type = property_type
             self.position = position
             self.price = price
             self.price_mortgage = price / 2
@@ -86,89 +79,93 @@ def get_board():
             self.rent_now = rent
             self.mortgage = 0
             self.owner = 0
-    
-    class Utility(Property):
-        def __init__(self, name, property_type, position, price, rent):
-            Property.__init__(self, name, property_type, position, price, rent)
-            self.rent_monopoly = rent + 6
-    
+
+    class Street(Property):
+        def __init__(self, name, position, color, price, price_building, rent,
+                     rent_building):
+                         Property.__init__(self, name, position, price, rent)
+                         self.color = color
+                         self.price_building = price_building
+                         self.rent_monopoly = rent * 2
+                         self.rent_building = rent_building
+                         self.n_building = 0
+
     class Railroad(Property):
-        def __init__(self, name, property_type, position, price, rent):
-            Property.__init__(self, name, property_type, position, price, rent)
+        def __init__(self, name, position, price, rent):
+            Property.__init__(self, name, position, price, rent)
             self.rent_double = rent * 2
             self.rent_triple = self.rent_double * 2
             self.rent_monopoly = self.rent_triple * 2
     
-    class Street(Property):
-        def __init__(self, name, property_type, position, price, rent, color,
-                     rent_building, price_building):
-                         Property.__init__(self, name, property_type, position,
-                                           price, rent)
-                         self.rent_monopoly = rent * 2
-                         self.color = color
-                         self.rent_building = rent_building
-                         self.price_building = price_building
-                         self.n_building = 0
+    class Utility(Property):
+        def __init__(self, name, position, price, rent):
+            Property.__init__(self, name, position, price, rent)
+            self.rent_monopoly = rent + 6
     
     class Tax(object):
-        def __init__(self, name, position, price):
-            self.name = name
-            self.position = position
+        def __init__(self, price):
             self.price = price
+
+    class Card(object):
+        pass  
     
-    class Space(object):
-        def __init__(self, name, position):
-            self.name = name
-            self.position = position    
+    class Chance(object):
+        pass
     
-    # Create a board
-    import csv
-    with open('board.csv', 'rU') as handle:
-        board_csv = csv.DictReader(handle)
-        board = []
-        for b in board_csv:
-            
-            # Properties
-            if b['type'] == 'street':
-                board.append(Street(b['name'], int(b['position']), b['color'],
-                                    int(b['price']), int(b['rent']),
-                                    int(b['rent_build_1']), int(b['rent_build_2']),
-                                    int(b['rent_build_3']), int(b['rent_build_4']),
-                                    int(b['rent_build_5']), int(b['cost_build'])))            
-            elif b['type'] == 'railroad':
-                board.append(Railroad(b['name'], int(b['position'])))
-            elif b['type'] == 'utility':
-                board.append(Utility(b['name'], int(b['position'])))
-            
-            # Tax
-            elif b['type'] == 'tax':
-                board.append(Tax(b['name'], int(b['position']), int(b['price'])))
-            
-            # Other
-            else:
-                board.append(Space(b['name'], int(b['position'])))
+    class Chest(object):
+        pass
+    
+    class Jail(object):
+        pass
+    
+    class Idle(object):
+        pass
+    
+    board = []
+    for r in csv.DictReader(open(board_file, 'rU')):
+        if r['Space'] == 'Street':
+            board.append(Street(r['Name'], int(r['Position']), r['Color'],
+                                int(r['Price']), int(r['PriceBuild']),
+                                int(r['Rent']), [int(r['RentBuild1']),
+                                int(r['RentBuild2']), int(r['RentBuild3']),
+                                int(r['RentBuild4']), int(r['RentBuild5'])]))
+        elif r['Space'] == 'Railroad':
+            board.append(Railroad(r['Name'], int(r['Position']),
+                                  int(r['Price']), int(r['Rent'])))
+        elif r['Space'] == 'Utility':
+            board.append(Utility(r['Name'], int(r['Position']),
+                                 int(r['Price']), int(r['Rent'])))
+        elif r['Space'] == 'Tax':
+            board.append(Tax(int(r['Price'])))
+        elif r['Space'] == 'Chance':
+            board.append(Chance())
+        elif r['Space'] == 'Chest':
+            board.append(Chest())
+        elif r['Space'] == 'Jail':
+            board.append(Jail())
+        elif r['Space'] == 'Idle':
+            board.append(Idle())
     
     return board
 
 
 
 def roll_dice():
-    
-    # Import module
-    from random import randint
 
     # Roll two six-sided die
-    return sum(randint(1, 6, 2))
+    return np.random.choice(np.arange(1, 7), 2)
 
 
 
 def main():
     
     # Declarations
-    n_players = 5 # Number of players
+    n_players = 5
+    board_file = '/Users/jmcontreras/GitHub/monopoly/board.csv'
     
     # Get board, players, and properties
     players = get_players(n_players)    
+    board = get_board(board_file)
     
     # Start the game
     while len(players) > 1:

c9f8493924e55425bc4200042dc181ffa5d4e7af
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2017-05-27 10:32:27 -0400

    Change evaluate_buy function to react_to_property_visit function.

--- monopoly.py
+++ monopoly.py
@@ -1,214 +1,239 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec 14 17:24:44 2013
-
-@author: jmcontreras
-"""
-
-# v 1.0 GET THE MECHANICS OF THE GAME GOING
-
-# This is what is happening so far:
-
-# (1) Players go around the board by rolling the dice. Doubles allow another
-#     turn and 3 doubles land a player in jail.
-# (2) Once in jail, players take the following strategy: use a get-out-of-jail  
-#     card if you have one, otherwise pay as soon as you can, otherwise roll
-
-# Next up
-# TODO: Allow a player without cash to sell their properties before defaulting
-#       Check the pay method of the Player object
-# TODO: Many of the classes (Chance, Chest, Idle) need work
-
-# v 2.0 MAKE THE GAME SMART
-
-# TODO: Players consider the state of the board before choosing a jail strategy
-
-
-# Import modules
-import csv
 import numpy as np
+import pandas as pd
+
+import config as c
 
 
+###########
+# CLASSES #
+###########
+
+class Player:
+
+    def __init__(self, player_id):
+        """Initialize player."""
+
+        self.id = player_id         # Identification number
+        self.cash = 1500            # Cash on hand
+        self.properties = []        # List of properties
+        self.position = 0           # Board position
+        self.jail_cards = 0         # Number of "Get Out Of Jail Free" cards
+        self.jail_turns = 0         # Number of remaining turns in jail
+        self.jail_strategy = None   # Jail strategy
+
+    def move(self, roll):
+        """Move forward on board."""
+
+        self.position += roll
+        if self.position >= 40:
+            self.position -= 40
+            self.cash += 200
+
+    def react_to_property_visit(self, players, prop):
+        """Decide whether to pay rent or buy property."""
+
+        prop_has_owner = prop.owner is not None
+        prop_has_no_mortgage = prop.mortgage is False
+        self_can_afford = self.cash >= prop.price
+
+        if prop_has_owner and prop_has_no_mortgage:
+            self.pay(players[prop.owner], prop.rent_now)
+
+        elif ~prop_has_owner and self_can_afford:
+            self.buy(prop)
+
+    def evaluate_build(self):
+        # Create a way to let players know when they have a monopoly
+        # Create a way also of letting players know how many buildings they have
+        # The second is a little tougher because you have to build them in a certain order
+        # At first, just choose randomly
+        # Then, they can start building their hotels as soon as they can
+        pass
+
+    def buy(self, property_):
+        """Acquire a property from the bank or another player. Pay for the property and update ownership."""
+
+        self.properties.append(property_.position)
+        self.cash -= property_.price
+        property_.owner = self.id
+
+        if c.VERBOSE:
+            print 'Player {} bought {} from {}'.format(self.id, property_.name, '')
+
+    def pay(self, other, payment):
+        """Pay for a property"""
+
+        self.cash -= payment
+        other.cash += payment
+
+        if c.VERBOSE:
+            print 'Player {} paid ${} to Player {}'.format(self.id, payment, other.id)
+
+    def go_to_jail(self):
+        """Send player to jail."""
+
+        self.position = 10
+        self.jail_turns = 3
+
+        if c.VERBOSE:
+            print 'Player {} goes to jail'.format(self.id)
+
+    def choose_jail_strategy(self, rolled_double):
+        """"""
+
+        if self.jail_cards > 0:
+            self.jail_strategy = 'card'
+            self.jail_turns = 0
+            self.jail_cards -= 1
+
+        elif self.cash >= 50:
+            self.jail_strategy = 'pay'
+            self.jail_turns = 0
+            self.cash -= 50
+
+        else:
+            self.jail_strategy = 'roll'
+            if rolled_double:
+                self.jail_turns = 0
+            else:
+                self.jail_turns -= 1
+                if self.jail_turns == 0:
+                    self.cash -= 50
+
+        if c.VERBOSE:
+            print 'Player {} took jail strategy {} and has {} turns left'.format(self.id, self.jail_strategy,
+                                                                                 self.jail_turns)
+
+    def default(self, players):
+
+        del players[self.id]
+
+
+class Property(object):
+    def __init__(self, name, position, price, rent):
+        self.name = name
+        self.position = position
+        self.price = price
+        self.price_mortgage = price / 2
+        self.rent = rent
+        self.rent_now = rent
+        self.mortgage = False
+        self.owner = None
+
+
+class Street(Property):
+    def __init__(self, name, position, color, price,
+                 price_building, rent, rent_building):
+        Property.__init__(self, name, position, price, rent)
+        self.color = color
+        self.price_building = price_building
+        self.rent_monopoly = rent * 2
+        self.rent_building = rent_building
+        self.n_building = 0
+
+
+class Railroad(Property):
+    def __init__(self, name, position, price, rent):
+        Property.__init__(self, name, position, price, rent)
+        self.rent_double = rent * 2
+        self.rent_triple = self.rent_double * 2
+        self.rent_monopoly = self.rent_triple * 2
+
+
+class Utility(Property):
+    def __init__(self, name, position, price, rent):
+        Property.__init__(self, name, position, price, rent)
+        self.rent_monopoly = rent + 6
+
+
+class Tax(object):
+    def __init__(self, price):
+        self.price = price
+
+
+class Card(object):
+    pass
+
+
+class Chance(object):
+    pass
+
+
+class Chest(object):
+    pass
+
+
+class Jail(object):
+    pass
+
+
+class Idle(object):
+    pass
+
+
+#############
+# FUNCTIONS #
+#############
 
 def get_players(n_players):
+    """Return object with a list of players, ensuring that there are between 2 to 8 players."""
 
-    # Ensure number of players is acceptable
-    if n_players < 2:
-        raise ValueError('A game must have at least 2 players.')
-    elif n_players > 8:
-        raise ValueError('A game must have no more than 8 players.')
-    
-    # Define player class
-    class Player(object):
-        
-        def __init__(self, player_id):
-            self.id = player_id  # Identification number
-            self.cash = 1500     # Cash on hand
-            self.properties = [] # List of properties
-            self.position = 0    # Board position
-            self.jail_cards = 0  # Number of "Get Out Of Jail Free" cards
-            self.jail_turns = 0  # Number of remaining turns in jail
-            self.jail_strtg = '' # Jail strategy
-        
-        # Move player across board
-        def move(self, roll, verbose=False):
-            self.position += roll
-            if self.position >= 40:
-                self.position -= 40
-                self.cash += 200
-            if verbose:
-                print 'Player {} to space: {}'.format(self.id, self.position)
-        
-        # Buy property
-        def buy(self, prop_id, price):
-            self.properties.append(prop_id)
-            self.cash -= price
-        
-        # Pay another player
-        def pay(self, payee, payment):
-            if self.cash > payment:
-                self.cash -= payment
-                players[payee].cash += payment
-            else:
-                players[payee].cash += self.cash
-                self.default(payee, payment)
-        
-        # Go to jail
-        def go_to_jail(self):
-            self.position = 10
-            self.jail_turns = 3
-        
-        # Choose jail strategy
-        def choose_jail_strtg(self, rolled_double):
-            if self.jail_cards > 0:
-                self.jail_strtg = 'card'
-                self.jail_turns = 0
-                self.jail_cards -= 1
-            elif self.cash >= 50:
-                self.jail_strtg = 'pay'
-                self.jail_turns = 0
-                self.cash -= 50
-            else:
-                self.jail_strtg = 'roll'
-                if rolled_double:
-                    self.jail_turns = 0
-                else:
-                    self.jail_turns -= 1
-                    if self.jail_turns == 0:
-                        self.cash -= 50
-        
-        # Default
-        def default(self):
-            del players[self.id]
-            
-    # Create and return players list
-    players = [Player(p) for p in xrange(n_players)]
-    return players
+    assert n_players >= 2 & n_players <= 8, 'A game must have at least 2 and no more than 8 players.'
 
+    return [Player(i) for i in range(n_players)]
 
 
 def get_board(board_file):
-    
-    class Property(object):
-        def __init__(self, name, position, price, rent):
-            self.name = name
-            self.position = position
-            self.price = price
-            self.price_mortgage = price / 2
-            self.rent = rent
-            self.rent_now = rent
-            self.mortgage = 0
-            self.owner = 0
+    """TODO"""
 
-    class Street(Property):
-        def __init__(self, name, position, color, price, price_building, rent,
-                     rent_building):
-                         Property.__init__(self, name, position, price, rent)
-                         self.color = color
-                         self.price_building = price_building
-                         self.rent_monopoly = rent * 2
-                         self.rent_building = rent_building
-                         self.n_building = 0
-
-    class Railroad(Property):
-        def __init__(self, name, position, price, rent):
-            Property.__init__(self, name, position, price, rent)
-            self.rent_double = rent * 2
-            self.rent_triple = self.rent_double * 2
-            self.rent_monopoly = self.rent_triple * 2
-    
-    class Utility(Property):
-        def __init__(self, name, position, price, rent):
-            Property.__init__(self, name, position, price, rent)
-            self.rent_monopoly = rent + 6
-    
-    class Tax(object):
-        def __init__(self, price):
-            self.price = price
-
-    class Card(object):
-        pass  
-    
-    class Chance(object):
-        pass
-    
-    class Chest(object):
-        pass
-    
-    class Jail(object):
-        pass
-    
-    class Idle(object):
-        pass
-    
     board = []
-    for r in csv.DictReader(open(board_file, 'rU')):
-        if r['Space'] == 'Street':
-            board.append(Street(r['Name'], int(r['Position']), r['Color'],
-                                int(r['Price']), int(r['PriceBuild']),
-                                int(r['Rent']), [int(r['RentBuild1']),
-                                int(r['RentBuild2']), int(r['RentBuild3']),
-                                int(r['RentBuild4']), int(r['RentBuild5'])]))
-        elif r['Space'] == 'Railroad':
-            board.append(Railroad(r['Name'], int(r['Position']),
-                                  int(r['Price']), int(r['Rent'])))
-        elif r['Space'] == 'Utility':
-            board.append(Utility(r['Name'], int(r['Position']),
-                                 int(r['Price']), int(r['Rent'])))
-        elif r['Space'] == 'Tax':
-            board.append(Tax(int(r['Price'])))
-        elif r['Space'] == 'Chance':
-            board.append(Chance())
-        elif r['Space'] == 'Chest':
-            board.append(Chest())
-        elif r['Space'] == 'Jail':
-            board.append(Jail())
-        elif r['Space'] == 'Idle':
-            board.append(Idle())
-    
+
+    for _, r in pd.read_csv(board_file).iterrows():
+        for case in Switch(r['class']):
+            if case('Street'):
+                board.append(Street(r['name'], r['position'], r['color'], r['price_buy'], r['price_build'], r['rent'],
+                                    [r['rent_build_1'], r['rent_build_2'], r['rent_build_3'], r['rent_build_4'],
+                                     r['rent_build_5']]))
+            elif case('Railroad'):
+                board.append(Railroad(r['name'], r['position'], r['price_buy'], r['rent']))
+            elif case('Utility'):
+                board.append(Utility(r['name'], r['position'], r['price_buy'], r['rent']))
+            elif case('Tax'):
+                board.append(Tax(r['price_buy']))
+            elif case('Chance'):
+                board.append(Chance())
+            elif case('Chest'):
+                board.append(Chest())
+            elif case('Jail'):
+                board.append(Jail())
+            elif case('Idle'):
+                board.append(Idle())
+
     return board
 
 
+def roll_dice(check_double=True):
+    """Roll two fair six-sided die.
 
-def roll_dice(check_double=True, verbose=False):
+    Attributes:
+    :param bool check_double: Specify whether roll_dice returns a boolean indicating if the roll was a double roll
+    :return int roll: Sum of the roll
+    :return bool roll_is_double: Indicate if the roll was a double roll
+    """
 
-    # Roll two six-sided die
     roll = np.random.choice(np.arange(1, 7), 2)
-    
-    # Report roll, if requested
-    if verbose:
+
+    if c.VERBOSE:
         print 'Die roll: {} and {}'.format(roll[0], roll[1])
-    
-    # Return roll sum and, if requested, double roll indicator
+
     if check_double:
         return roll.sum(), roll[0] == roll[1]
     else:
         return roll.sum()
 
 
+class Switch(object):
 
-
-class switch(object):
     def __init__(self, value):
         self.value = value
         self.fall = False
@@ -217,12 +242,12 @@ class switch(object):
         """Return the match method once, then stop"""
         yield self.match
         raise StopIteration
-    
+
     def match(self, *args):
         """Indicate whether or not to enter a case suite"""
         if self.fall or not args:
             return True
-        elif self.value in args: # changed for v1.5, see below
+        elif self.value in args:
             self.fall = True
             return True
         else:
@@ -230,72 +255,71 @@ class switch(object):
 
 
 def main():
-    
-    # Declarations
-    n_players = 5
-    board_file = '/Users/jmcontreras/GitHub/monopoly/board.csv'
-    
-    # Get players and board (including properties)
-    players = get_players(n_players)
-    board = get_board(board_file)
-    
-    game_round = 1    
-    
-    # Start game
+
+    # Create object with players
+    players = get_players(c.N_PLAYERS)
+
+    # Create board with properties
+    board = get_board(c.BOARD_FILENAME)
+
+    # Continue playing as long as more than one player remains in game
     while len(players) > 1:
-        
+
         # Take turns
-        for turn in range(n_players):
-            
+        for turn in range(c.N_PLAYERS):
+
+            # Define current player
+            curr_player = players[turn]
+
             # Double roll counter
             n_double_roll = 0
-            
+
             # Continue turn until player rolls no doubles or goes to jail
             while True:
-                
+
                 # Roll dice
-                roll, rolled_double = roll_dice(verbose=False)
+                roll, rolled_double = roll_dice()
 
                 # Update double roll counter
-                n_double_roll += (rolled_double).astype(int)
-                
+                n_double_roll += int(rolled_double)
+
                 # If player is in jail
                 if players[turn].jail_turns > 0:
-                    
+
                     # Select jail strategy
-                    players[turn].choose_jail_strtg(rolled_double)
-                    
+                    curr_player.choose_jail_strategy(rolled_double)
+
                     # If player is still in jail
-                    if players[turn].jail_turns > 0:
+                    if curr_player.jail_turns > 0:
                         break
-                
+
                 # If player rolled less than 3 doubles
                 if n_double_roll < 3:
-                    
-                    # Move player 
-                    players[turn].move(roll, verbose=False)
+
+                    # Move player
+                    curr_player.move(roll)
+
+                    # Define current board space
+                    curr_space = board[curr_player.position]
+
+                    for case in Switch(type(curr_space).__name__):
+                        if case('Street'):
+                            curr_player.evaluate_buy(curr_space, players)
 
                     # If no double rolled, end turn
                     if not rolled_double:
                         break
-                
-                # Otherwise, send player to jail and end turn
-                else:
-                    players[turn].go_to_jail()
-                    break
-                
-                # Now here is where we start interacting with the board
-                type(board[4]).__name__
-                http://code.activestate.com/recipes/410692/
-        
-        game_round += 1      
-        
-        if game_round == 20:
-            break
-        
-        # sum values across list
-        # sum(players[i].cash for i in range(0,4))
 
-if __name__ == '__main__':
-    
-    main()
+                # Otherwise, send player to jail and end turn
+                elif n_double_roll == 3:
+
+                    curr_player.go_to_jail()
+                    break
+
+                    # Now here is where we start interacting with the board
+                    # type(board[4]).__name__
+
+        game_round += 1
+
+        if game_round == 10:
+            break

951cb44a9446220e21e09789feef89fc88e290cf
Juan Manuel Contreras <jm-contreras@users.noreply.github.com>
2017-05-31 19:28:20 -0400

    Update utils documentation and formatting.

--- src/utils.py
+++ src/utils.py
@@ -5,36 +5,68 @@ import classes
 
 
 def get_players(n_players):
-    """Return object with a list of players, ensuring that there are between 2 to 8 players."""
+    """Return dictionary with list of 2 to 8 players.
 
-    assert n_players >= 2 & n_players <= 8, 'A game must have at least 2 and no more than 8 players.'
+    Parameter
+    ---------
+    n_players : int
+        Number of players in game
 
-    return [classes.Player(i) for i in range(n_players)]
+    Return
+    ------
+    dict
+        Dictionary of players
+    """
+
+    if n_players < 2 or 8 < n_players:
+        raise ValueError('A game must have between 2 to 8 players. You input {} players.'.format(n_players))
+
+    return {classes.Player(p) for p in range(n_players)}
 
 
 def get_board(board_file):
-    """TODO"""
+    """Return game board.
+
+    Parameter
+    ---------
+    board_file : str
+        Filename of CSV with board parameters
+
+    Return
+    ------
+    board: list
+        Board game with one item per space
+    """
 
     board = []
 
     for _, r in pd.read_csv(board_file).iterrows():
+
         for case in classes.Switch(r['class']):
+
             if case('Street'):
                 board.append(classes.Street(r['name'], r['position'], r['color'], r['price_buy'], r['price_build'],
                                             r['rent'], [r['rent_build_1'], r['rent_build_2'], r['rent_build_3'],
                                                         r['rent_build_4'], r['rent_build_5']]))
+
             elif case('Railroad'):
                 board.append(classes.Railroad(r['name'], r['position'], r['price_buy'], r['rent']))
+
             elif case('Utility'):
                 board.append(classes.Utility(r['name'], r['position'], r['price_buy'], r['rent']))
+
             elif case('Tax'):
                 board.append(classes.Tax(r['price_buy']))
+
             elif case('Chance'):
                 board.append(classes.Chance())
+
             elif case('Chest'):
                 board.append(classes.Chest())
+
             elif case('Jail'):
                 board.append(classes.Jail())
+
             elif case('Idle'):
                 board.append(classes.Idle())
 
@@ -44,10 +76,17 @@ def get_board(board_file):
 def roll_dice(check_double=True):
     """Roll two fair six-sided die.
 
-    Attributes:
-    :param bool check_double: Specify whether roll_dice returns a boolean indicating if the roll was a double roll
-    :return int roll: Sum of the roll
-    :return bool roll_is_double: Indicate if the roll was a double roll
+    Parameter
+    ---------
+    check_double : bool
+        Specify whether return should include boolean indicating if roll was double (defaults to True)
+
+    Returns
+    -------
+    roll: int
+        Sum of roll
+    roll_is_double: bool
+        Indicate if roll was double
     """
 
     roll = np.random.choice(np.arange(1, 7), 2)

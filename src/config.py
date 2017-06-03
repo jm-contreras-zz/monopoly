ee3e45fa073d2d1fe4bfb2835a4dc55c99225eed
Juan Manuel Contreras <jm-contreras@users.noreply.github.com>
2017-06-03 09:39:55 -0500

    Document config.

--- src/config.py
+++ src/config.py
@@ -1,4 +1,3 @@
-VERBOSE = True
-N_PLAYERS = 5
-ROUND = 1
-BOARD_FILENAME = 'board.csv'
+N_PLAYERS = 5                   # Number of players
+ROUND = 1                       # Game round
+BOARD_FILENAME = 'board.csv'    # Board filename
